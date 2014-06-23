94afb3dc3f41c291062b00f7c7551a8c48549ebd
hydai <z54981220@gmail.com>
2014-06-23 16:46:59 +0800

    add math facts

--- Generales/vimrc.cpp
+++ Generales/vimrc.cpp
@@ -2,8 +2,6 @@ set ts=4
 set bs=2		" allow backspacing over everything in insert mode
 set ruler		" show the cursor position all the time
 set nu
-filetype on           " Enable filetype detection
-filetype indent on    " Enable filetype-specific indenting
 syntax on		" syntax highlight
 set hlsearch		" search highlighting
 set showmatch		" Cursor shows matching ) and }
@@ -13,15 +11,9 @@ set incsearch		" incremental search
 set nobackup		" no *~ backup files
 set copyindent		" copy the previous indentation on autoindenting
 
-"--------------------------------------------------------------------------- 
-" USEFUL SHORTCUTS
-"--------------------------------------------------------------------------- 
 " set leader to ,
 let mapleader=","
 let g:mapleader=","
-
-"replace the current word in all opened buffers
-map <leader>r :call Replace()<CR>
 
 " ,/ turn off search highlighting
 nmap <leader>/ :nohl<CR>
