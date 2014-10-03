e293e585675eecfaefec54af2a217f90b217c55e
Andy Wang <andyw330@gmail.com>
2014-10-04 03:14:49 +0800

    add AP code

---
+++ Graph/AP.cpp
@@ -0,0 +1,28 @@
+#define MAXN 10001
+
+vector<int> v[MAXN]; //adjacency list
+bool rec[MAXN];
+int clk[MAXN], low[MAXN], time, ct; //ct: 計算AP個數
+
+void dfs(int x, int parent){
+	rec[x] = 1;
+	clk[x] = low[x] = ++time;
+	int child = 0;
+	bool ap = 0;
+	for(int i = 0; i < v[x].size(); i++){
+		int y = v[x][i];
+		if(y != p){
+			if(!rec[y]){
+				child++;
+				dfs(y, x);
+				low[x] = low[x] < low[y] ? low[x] : low[y];
+				if(low[y] >= clk[x])
+					ap = 1;
+			}
+			else
+				low[x] = low[x] < clk[y] ? low[x] : clk[y];
+		}
+	}
+	if(x == parent && child > 1 || x != parent && ap)
+		ct++; //第 x 點是關節點";
+}
